11104e3d891659a6b1ea29353e2047b0ce8b3454
Laura Sofia Tamayo Pelaez <ltamayo115406@umanizales.edu.co>
2026-08-19 20:30:15 -0500

    Add BigDecimal import and initialize Cuenta

--- main.java
+++ main.java
@@ -1,6 +1,12 @@
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("Sistema de pagos");
+
+        Cuenta cuenta = new Cuenta("001", new BigDecimal("100000"));
+
+        System.out.println("Saldo inicial: " + cuenta.getSaldo());
     }
 }
