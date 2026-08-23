0fee52cbbd42b2758e553fc013542a899aceac2f
Laura Sofia Tamayo Pelaez <ltamayo115406@umanizales.edu.co>
2026-08-22 21:36:52 -0500

    Implement PagoEfectivo class for cash payments

--- PagoEfectivo.java
+++ PagoEfectivo.java
@@ -1,1 +1,14 @@
+import java.math.BigDecimal;
 
+public class PagoEfectivo implements Pago {
+
+    @Override
+    public void procesar(BigDecimal monto) {
+        System.out.println("Registrando pago en efectivo de $" + monto);
+    }
+
+    @Override
+    public String getDescripcion() {
+        return "Efectivo";
+    }
+}
