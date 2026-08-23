6167d8420737968e6f69c2e9c164de6f4d869046
Laura Sofia Tamayo Pelaez <ltamayo115406@umanizales.edu.co>
2026-08-22 21:40:41 -0500

    Update CheckoutService.java

--- CheckoutService.java
+++ CheckoutService.java
@@ -1,1 +1,11 @@
+public class CheckoutService {
 
+    public void finalizarCompra(Pedido pedido, Pago metodoDePago) {
+
+        metodoDePago.procesar(pedido.getTotal());
+
+        System.out.println(
+            "Compra finalizada. Método: " + metodoDePago.getDescripcion()
+        );
+    }
+}
