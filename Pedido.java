bd7e0817ab0e9215f8a0cdc55ca31816f6d998f3
Laura Sofia Tamayo Pelaez <ltamayo115406@umanizales.edu.co>
2026-08-22 21:23:28 -0500

    Add Pedido class with total property and getter

--- Pedido.java
+++ Pedido.java
@@ -1,1 +1,14 @@
+import java.math.BigDecimal;
 
+public class Pedido {
+
+    private BigDecimal total;
+
+    public Pedido(BigDecimal total) {
+        this.total = total;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+}
