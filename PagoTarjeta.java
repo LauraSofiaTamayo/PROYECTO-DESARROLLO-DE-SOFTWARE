c81aebf9900c56ce8984c601f7c3bcb576e4ec60
Laura Sofia Tamayo Pelaez <ltamayo115406@umanizales.edu.co>
2026-08-22 21:32:09 -0500

    Add PagoTarjeta class for processing card payments
    Implement PagoTarjeta class for card payments.

--- PagoTarjeta.java
+++ PagoTarjeta.java
@@ -1,1 +1,20 @@
+import java.math.BigDecimal;
 
+public class PagoTarjeta implements Pago {
+
+    private final String numeroTarjeta;
+
+    public PagoTarjeta(String numeroTarjeta) {
+        this.numeroTarjeta = numeroTarjeta;
+    }
+
+    @Override
+    public void procesar(BigDecimal monto) {
+        System.out.println("Cargando $" + monto + " a la tarjeta " + numeroTarjeta);
+    }
+
+    @Override
+    public String getDescripcion() {
+        return "Tarjeta " + numeroTarjeta;
+    }
+}
